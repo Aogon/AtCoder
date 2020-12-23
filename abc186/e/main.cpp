868dc5fb352eda2a2745f5cca88288f695ddbcbe
Aogon <aoi_sakai@icloud.com>
2020-12-23 09:55:31 +0900

    [add]ABC186Eの追加

--- abc186/e/main.cpp
+++ abc186/e/main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+    int t;
+    long long int n, s, k;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n >> s >> k;
+        k = k % n;
+        
+    }
+    
     return 0;
 }
